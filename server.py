cf2bd97ffe34cef29583c4ebab397db6b43e73c0
wgus1801 <76962731+wgus1801@users.noreply.github.com>
2022-02-12 13:59:14 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,36 +3,57 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-balance = 100
-transactions = []
+value = 0
+calculations = []
 
-@app.route('/atm/getBalance',methods = ['GET'])
-def getBalance():
-   return jsonify({"balance":balance});
+@app.route('/calc/getValue',methods = ['GET'])
+def getValue():
+   return jsonify({"value":value});
 
-@app.route('/atm/getHistory',methods = ['GET'])
-def getHistory():
-   return jsonify(transactions);
+@app.route('/calc/getComputations',methods = ['GET'])
+def getComputations():
+   return jsonify(calculations);
 
-@app.route('/atm/deposit',methods = ['POST'])
-def deposit():
-   global balance
-   transaction = request.get_json()
-   transaction["type"] = "deposit"
-   transaction["time"] = datetime.now()
-   balance = balance + transaction["amount"]
-   transactions.append(transaction)
-   return jsonify({"balance":balance});
+@app.route('/calc/add',methods = ['POST'])
+def add():
+   global value
+   calculation = request.get_json()
+   calculation["type"] = "add"
+   calculation["time"] = datetime.now()
+   value = value + calculation["amount"]
+   calculations.append(calculation)
+   return jsonify({"value":value});
 
-@app.route('/atm/withdraw',methods = ['POST'])
-def withdraw():
-   global balance
-   transaction = request.get_json()
-   transaction["type"] = "withdraw"
-   transaction["time"] = datetime.now()
-   balance = balance - transaction["amount"]
-   transactions.append(transaction)
-   return jsonify({"balance":balance});
+@app.route('/calc/subtract',methods = ['POST'])
+def subtract():
+   global value
+   calculation = request.get_json()
+   calculation["type"] = "subtract"
+   calculation["time"] = datetime.now()
+   value = value - calculation["amount"]
+   calculations.append(calculation)
+   return jsonify({"value":value});
+
+@app.route('/calc/multiply',methods = ['POST'])
+def multiply():
+   global value
+   calculation = request.get_json()
+   calculation["type"] = "multiply"
+   calculation["time"] = datetime.now()
+   value = value * calculation["amount"]
+   calculations.append(calculation)
+   return jsonify({"value":value});
+
+@app.route('/calc/divide',methods = ['POST'])
+def divide():
+   global value
+   calculation = request.get_json()
+   calculation["type"] = "divide"
+   calculation["time"] = datetime.now()
+   value = value / calculation["amount"]
+   calculations.append(calculation)
+   return jsonify({"value":value});
+
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=8081, debug = True)
